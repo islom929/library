6d39e09c962922de885836049bd6f29fab67bf19
islom929 <islomsultonov0@gmail.com>
2021-09-24 09:20:39 +0500

    smth changed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// FORM VARS
+const elForm = document.querySelector('.js-search-form');
+const elFormInputTitle = document.querySelector('.js-search-input-title');
+const elFormInputAuthor = document.querySelector('.js-search-input-author');
+const elFormInputCountry = document.querySelector('.js-search-input-country');
+const elFormInputYear = document.querySelector('.js-search-input-year');
+const elFormInputSubmit = document.querySelector('.js-search-input-submit');
+
+// RESULT
+const elLibraryList = document.querySelector('.books__list');
+
+
